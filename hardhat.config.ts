6bd599c86dc96e4bfbc1210898ceed14c5c572f2
Jona630 <jonathan.a.allegretti@gmail.com>
2024-01-31 17:41:24 -0300

    Update hardhat.config.ts and package.json
    This commit updates the hardhat.config.ts file to use environment variables for INFURA_API_KEY, SEPOLIA_ACCOUNT_KEY, and ETHERSCAN_API_KEY. It also adds the dotenv package as a dependency in the package.json file.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,25 +1,30 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
-import '@openzeppelin/hardhat-upgrades';
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 
-const INFURA_API_KEY = 'ef5c71f0aa144eb89fbef447df6df86c';
-const SEPOLIA_PRIVATE_KEY =
-  'bb57627e6c73b139615942280dc9464d9f7e5694a2ff4ab05b2ec3496e02f176';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const SEPOLIA_ACCOUNT_KEY = process.env.SEPOLIA_ACCOUNT_KEY
+	? [process.env.SEPOLIA_ACCOUNT_KEY]
+	: [""];
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.20',
-  defaultNetwork: 'hardhat',
-  networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: 'QVK5GU9IX34S142NUV2ITBTF7NQJ8DDJZU',
-    },
-  },
+	solidity: "0.8.20",
+	defaultNetwork: "hardhat",
+	networks: {
+		sepolia: {
+			url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+			accounts: SEPOLIA_ACCOUNT_KEY,
+		},
+	},
+	etherscan: {
+		apiKey: {
+			sepolia: ETHERSCAN_API_KEY,
+		},
+	},
 };
 
 export default config;
