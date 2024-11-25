d053e074f32b7aa137dcf917c53bad0d1dda547c
Santiago Grangetto <sgrangetto@spacedev.io>
2024-11-25 18:19:51 -0300

    Add unit test for update metadata and add prettier for ts files

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  overrides: [
+    {
+      files: "*.ts",
+      options: {
+        semi: true,
+        trailingComma: "all",
+        printWidth: 80,
+        tabWidth: 2,
+      },
+    },
+  ],
+};
