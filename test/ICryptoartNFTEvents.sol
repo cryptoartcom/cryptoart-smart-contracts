ddedbfa896fbb2bfecf8eee670b23093821351c8
ksmit323 <ksmit323@gmail.com>
2025-03-21 14:03:40 +0700

    add admin tests with events

---
+++ test/ICryptoartNFTEvents.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface ICryptoartNFTEvents {
+    event Initialized(address contractOwner, address contractAuthoritySigner);
+    event BaseURISet(string newBaseURI);
+    event MaxSupplySet(uint256 newMaxSupply);
+    event RoyaltiesUpdated(address indexed receiver, uint256 newPercentage);
+    event AuthoritySignerUpdated(address newAuthoritySigner);
+    event NftReceiverUpdated(address newNftReceiver);
+    event ToggleStoryVisibility(uint256 tokenId, string storyId, bool visible);
+
+    // NFT lifecycle events
+    event Minted(address indexed recipient, uint256 tokenId);
+    event MintedByBurning(uint256 tokenId, uint256[] burnedTokenIds);
+    event Claimed(uint256 tokenId);
+    event Burned(uint256 tokenId);
+    event MintedByTrading(uint256 newTokenId, uint256[] tradedTokenIds); 
+}
